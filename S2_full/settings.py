cc0d6edabdd5f3282b43dfb87e4bc7ca592cd897
discegaudere113399 <discegaudere113399@gmail.com>
2024-05-13 01:03:43 +0000

    server first commit

--- S2_full/settings.py
+++ S2_full/settings.py
@@ -130,12 +130,12 @@ WSGI_APPLICATION = 'S2_full.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -169,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static/"
 
 MEDIA_URL = "/media/"
